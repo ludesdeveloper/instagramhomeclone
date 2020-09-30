716cd6e16eccdc79d59cd82f9adf1dc829a40cc9
Seski Ramadhan <seski.ramadhan@gmail.com>
2020-09-30 11:36:15 +0700

    instagram clone

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          Text('data'),
+          Text('data'),
+          Text('data'),
+        ],
+      ),
+    );
+  }
+}
